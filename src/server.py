3fa9c78c2b09dd87c64311d1a55af1ba0b0bbbec
Hamid Zare <dellydela@gmail.com>
2019-06-09 21:28:28 -0700

    updated server to serve static files from public dir

--- src/server.py
+++ src/server.py
@@ -1,8 +1,12 @@
 # flask web server
 
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='../public')
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def root():
+  return app.send_static_file('index.html')
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
