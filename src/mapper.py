fdfd5676d11cbe764494c9ce94f75f3631c04294
Hamid Zare <dellydela@gmail.com>
2019-06-09 18:52:40 -0700

    refactored mapper and reduce into composable modules

--- src/mapper.py
+++ src/mapper.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 
-import sys
+def mapper(lines):
+  for line in lines:
+    line = line.strip()
+    words = line.split()
+    for word in words:
+      yield (word, 1) # emit the word
 
-for line in sys.stdin:
-  line = line.strip()
-  words = line.split()
-  for word in words:
-    print(word, 1) # emit the word
+
+if __name__ == '__main__':
+  import sys
+  import argparse
+
+  parser = argparse.ArgumentParser(description="file to word mapper")
+  parser.add_argument('--input', help='input file')
+
+  args = parser.parse_args()
+
+  with open(args.input, 'r') as f:
+    for word, count in mapper(f):
+      print(word, count)
--- src/reducer.py
+++ src/reducer.py
@@ -2,23 +2,25 @@
 
 import sys
 
-current_word = None
-current_count = 0
-word = None
+# WARN requires sorted input
+def reducer(mappedWords):
 
-# input comes from STDIN
-for line in sys.stdin:
-  line = line.strip()
-  word, count = line.split(' ', 1)
-  count = int(count)
+  current_word = None
+  current_count = 0
+  word = None
+
+  for line in lines:
+    line = line.strip()
+    word, count = line.split(' ', 1)
+    count = int(count)
+    if current_word == word:
+      current_count += count
+    else:
+      if current_word != None:
+        yield (current_word, current_count)
+      current_count = count
+      current_word = word
+
+  # don't miss the last word
   if current_word == word:
-    current_count += count
-  else:
-    if current_word != None:
-      print(current_word, current_count)
-    current_count = count
-    current_word = word
-
-# don't miss the last word
-if current_word == word:
-  print(current_word, current_count)
+    yield (current_word, current_count)
